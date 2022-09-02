7e3db6407573fbd1c7cecf52e797f0006493c7fd
Bob <731653765@qq.com>
2022-09-02 22:34:14 +0800

    feat: introduce the style component and make application response to mobile

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-
-import './App.css';
+import GlobalStyle from './global.styles';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -47,7 +46,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <Header /> */}
+        <GlobalStyle />
         <HeaderContainer />
         <Switch>
           <Route exact path='/' component={HomePage} />
